7f0ed1b544d78cb1d5d6395884e3bdde1bc758b3
Pedro Amaral <30008241@students.ual.pt>
2022-04-19 20:56:23 +0100

    Lab2 done - Before official tests.

---
+++ views/cli.py
@@ -0,0 +1,45 @@
+from models.LinkedList import LinkedList
+
+
+
+def main():
+    lista_ligada = LinkedList()
+    while True:
+        try:
+            comandos = input().split(" ")
+        except EOFError:
+            return
+        match comandos[0]:
+            case "RPI":
+                lista_ligada.insert_at_start(comandos[1])
+                lista_ligada.traverse_list()
+            case "RPF":
+                lista_ligada.insert_at_end(comandos[1])
+                lista_ligada.traverse_list()
+            case "RPDE":
+                lista_ligada.insert_after_item(comandos[1],comandos[2])
+            case "RPAE":
+                lista_ligada.insert_before_item(comandos[1],comandos[2])
+            case "RPII":
+                lista_ligada.insert_at_index(int(comandos[1]), comandos[2])
+            case "VNE":
+                print(f"O numero de elementos é {lista_ligada.get_count()}.")
+            case "VP":
+                if lista_ligada.search_item(comandos[1]) == True:
+                    print(f"O país {comandos[1]} encontra-se na lista.")
+                else:
+                    print(f"O país {comandos[1]} não se encontra na lista.")
+            case "EPE":
+                print(f"O país {lista_ligada.start_node.get_item()} foi eliminado da lista.")
+                lista_ligada.delete_at_start()
+            case "EUE":
+                print(f"O país {lista_ligada.get_last_node()} foi eliminado.")
+                lista_ligada.delete_at_end()
+            case "EP":
+                lista_ligada.delete_element_by_value(comandos[1])
+            case "PL":
+                lista_ligada.traverse_list()
+            case "":
+                break
+            case default:
+                pass
